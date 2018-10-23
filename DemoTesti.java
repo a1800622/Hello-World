977aa56879a960fe887d91dddc11d029ea25ecad
a1800622 <44391805+a1800622@users.noreply.github.com>
2018-10-23 11:18:19 +0300

    Add files via upload
    Testing

---
+++ DemoTesti.java
@@ -0,0 +1,21 @@
+package junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class DemoTesti {
+	
+	@Test
+	
+	public void testiHello(){
+		
+	String moi = "Hello World";
+	
+	
+	
+	assertEquals("Hello World",moi);
+}
+}
